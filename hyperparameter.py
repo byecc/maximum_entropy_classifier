d068460d807de1177814b6cda17606a538aead9f
wangmingtao <1026259343@qq.com>
2017-11-16 09:26:32 +0800

    add ap_iter parameter

--- hyperparameter.py
+++ hyperparameter.py
@@ -1,6 +1,8 @@
 class Parameter:
     def __init__(self):
-        self.learn_rate = 0.5
+        self.learn_rate = 0.1
         self.batch_size = 45
         self.iter_num = 512
-        self.kthi = 1e-10
+        self.kthi = 0.001
+        self.class_num = 5
+        self.ap_iter_num = 50
